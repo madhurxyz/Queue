28baf1a6c58e1ad3b5f0ee9bbd7b8a307070bea9
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-01 13:07:37 -0800

    trying to implement with LinkedList

--- queue.py
+++ queue.py
@@ -1,12 +1,52 @@
 #!python
-# from linkedlist import LinkedList
+from linkedlist import LinkedList
 
+# class Queue(object):
+#
+#     def __init__(self, iterable=None):
+#         """Initialize this queue and enqueue the given items, if any"""
+#         self.data = list()
+#         if iterable:
+#             for item in iterable:
+#                 self.enqueue(item)
+#
+#     def __repr__(self):
+#         """Return a string representation of this queue"""
+#         return 'Queue({})'.format(self.length())
+#
+#     def is_empty(self):
+#         """Return True if this queue is empty, or False otherwise"""
+#         return not self.data
+#
+#     def length(self):
+#         """Return the number of items in this queue"""
+#         return len(self.data)
+#
+#     def peek(self):
+#         """Return the next item in this queue without removing it,
+#         or None if this queue is empty"""
+#         if self.is_empty():
+#             return None
+#         return self.data[0]
+#
+#     def enqueue(self, item):
+#         """Enqueue the given item into this queue"""
+#         self.data.append(item)
+#
+#     def dequeue(self):
+#         """Return the next item and remove it from this queue,
+#         or raise ValueError if this queue is empty"""
+#         if self.is_empty():
+#             raise ValueError
+#         next_item = self.peek()
+#         del self.data[0]
+#         return next_item
 
 class Queue(object):
 
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any"""
-        self.data = list()
+        super(Queue, self).__init__()
         if iterable:
             for item in iterable:
                 self.enqueue(item)
