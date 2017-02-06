84aff4ee490a4c8c8ddc9b27519a95c8a3667d69
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-06 07:43:36 -0800

    refactored queue with list implementation, all tests passed

--- queue.py
+++ queue.py
@@ -1,56 +1,15 @@
 #!python
 from linkedlist import LinkedList
 
-# class Queue(list):
-#     # enqueue - append to end or append to front
-#     # dequeue - remove from front or remove from end
-#     # peek - examine front
-#     def __init__(self, iterable=None):
-#         """Initialize this queue and enqueue the given items, if any"""
-#         super(Queue, self).__init__()
-#         if iterable:
-#             for item in iterable:
-#                 self.enqueue(item)
-#
-#     def __repr__(self):
-#         """Return a string representation of this queue"""
-#         return 'Queue({})'.format(self.length())
-#
-#     def is_empty(self):
-#         """Return True if this queue is empty, or False otherwise"""
-#         return len(self) == 0
-#
-#     def length(self):
-#         """Return the number of items in this queue"""
-#         return len(self)
-#
-#     def peek(self):
-#         """Return the next item in this queue without removing it,
-#         or None if this queue is empty"""
-#         if self.is_empty():
-#             return None
-#         return self[0]
-#
-#     def enqueue(self, item):
-#         """Enqueue the given item into this queue"""
-#         self.append(item)
-#
-#     def dequeue(self):
-#         """Return the next item and remove it from this queue,
-#         or raise ValueError if this queue is empty"""
-#         if self.is_empty():
-#             raise ValueError
-#         else:
-#             self.pop(0)
-
-class Queue(LinkedList):
+class Queue(object):
 
     #enqueue - append to tail
     #dequeue - remove from head
     #peek - examine first
+    
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any"""
-        super(Queue, self).__init__()
+        self.queue = []
         if iterable:
             for item in iterable:
                 self.enqueue(item)
@@ -61,23 +20,24 @@ class Queue(LinkedList):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise"""
-        return super(Queue, self).is_empty()
+        if len(self.queue) == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this queue"""
-        return self.size
+        return len(self.queue)
 
     def peek(self):
         """Return the next item in this queue without removing it,
         or None if this queue is empty"""
         if self.is_empty():
             return None
-        else:
-            return self[0]
+        return self.queue[0]
 
     def enqueue(self, item):
         """Enqueue the given item into this queue"""
-        self.append(item)
+        self.queue.append(item)
 
     def dequeue(self):
         """Return the next item and remove it from this queue,
@@ -85,6 +45,4 @@ class Queue(LinkedList):
         if self.is_empty():
             raise ValueError
         else:
-            first_item = self.data.head.data
-            self.delete(first_item)
-            return first_item
+            return self.queue.pop(0)
