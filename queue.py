ca3deec4fc3fd0b49fb3ef19c28d893ed1f8ead6
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-06 08:00:02 -0800

    added complexity analysis

--- queue.py
+++ queue.py
@@ -1,11 +1,12 @@
 #!python
 from linkedlist import LinkedList
 
+# enqueue - append to tail
+# dequeue - remove from head
+# peek - examine first
+
+
 # class Queue(object):
-#
-#     #enqueue - append to tail
-#     #dequeue - remove from head
-#     #peek - examine first
 #
 #     def __init__(self, iterable=None):
 #         """Initialize this queue and enqueue the given items, if any"""
@@ -18,16 +19,19 @@ from linkedlist import LinkedList
 #         """Return a string representation of this queue"""
 #         return 'Queue({})'.format(self.length())
 #
+#     #O(1)
 #     def is_empty(self):
 #         """Return True if this queue is empty, or False otherwise"""
 #         if len(self.queue) == 0:
 #             return True
 #         return False
 #
+#     #O(1)
 #     def length(self):
 #         """Return the number of items in this queue"""
 #         return len(self.queue)
 #
+#     #O(1)
 #     def peek(self):
 #         """Return the next item in this queue without removing it,
 #         or None if this queue is empty"""
@@ -35,10 +39,12 @@ from linkedlist import LinkedList
 #             return None
 #         return self.queue[0]
 #
+#     #O(1)
 #     def enqueue(self, item):
 #         """Enqueue the given item into this queue"""
 #         self.queue.append(item)
 #
+#     #O(1)
 #     def dequeue(self):
 #         """Return the next item and remove it from this queue,
 #         or raise ValueError if this queue is empty"""
@@ -48,10 +54,6 @@ from linkedlist import LinkedList
 #             return self.queue.pop(0)
 
 class Queue(object):
-
-    #enqueue - append to tail
-    #dequeue - remove from head
-    #peek - examine first
 
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any"""
@@ -64,16 +66,19 @@ class Queue(object):
         """Return a string representation of this queue"""
         return 'Queue({})'.format(self.length())
 
+    #O(1)
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise"""
         if self.queue.length() == 0:
             return True
         return False
 
+    #O(1)
     def length(self):
         """Return the number of items in this queue"""
         return self.queue.length()
 
+    #O(1)
     def peek(self):
         """Return the next item in this queue without removing it,
         or None if this queue is empty"""
@@ -81,10 +86,12 @@ class Queue(object):
             return None
         return self.queue.head.data
 
+    #O(1)
     def enqueue(self, item):
         """Enqueue the given item into this queue"""
         self.queue.append(item)
 
+    #O(n)
     def dequeue(self):
         """Return the next item and remove it from this queue,
         or raise ValueError if this queue is empty"""
